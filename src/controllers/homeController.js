15092593f85c5da98b1bfeb0e13e8c5162137bb5
NguyenVu <nguyenvu130705@gmail.com>
2025-09-10 22:52:03 +0700

    Create userController & userService

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -28,9 +28,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('----------------');
-    console.log(data);
-    console.log('----------------');
     return res.render('displayCRUD.ejs', {
         dataTable: data
     });
@@ -59,6 +56,13 @@ let putCRUD = async (req, res) => {
     });
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+
+    await CRUDService.deleteUserById(id);
+    return res.send('Delete the user succeed!');
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
@@ -66,5 +70,6 @@ module.exports = {
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
-    putCRUD: putCRUD
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
